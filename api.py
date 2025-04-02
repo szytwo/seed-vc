959475e8b646e7c8add94eaee8008faf641c840e
Administrator <szytwo@qq.com>
2025-04-02 23:45:29 +0800

    Changes

--- api.py
+++ api.py
@@ -187,18 +187,16 @@ def clear_cuda_cache():
     """
     if torch.cuda.is_available():
         logging.info("Clearing GPU memory...")
-        
+
         torch.cuda.empty_cache()
         torch.cuda.ipc_collect()
-
+        # 重置统计信息
+        torch.cuda.reset_peak_memory_stats()
         # 打印显存日志
         logging.info(f"[GPU Memory] Allocated: {torch.cuda.memory_allocated() / (1024 ** 2):.2f} MB")
         logging.info(f"[GPU Memory] Max Allocated: {torch.cuda.max_memory_allocated() / (1024 ** 2):.2f} MB")
         logging.info(f"[GPU Memory] Reserved: {torch.cuda.memory_reserved() / (1024 ** 2):.2f} MB")
         logging.info(f"[GPU Memory] Max Reserved: {torch.cuda.max_memory_reserved() / (1024 ** 2):.2f} MB")
-
-        # 重置统计信息
-        torch.cuda.reset_peak_memory_stats()
 
 
 # 设置允许访问的域名
